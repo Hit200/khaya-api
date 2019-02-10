2ff6ade6e9141fe94525ed3d458e5318e23e1c0c
Conceptx <theophilusokoye6@gmail.com>
2019-02-11 00:16:35 +0200

    signup with REST response

--- index.js
+++ index.js
@@ -1,33 +1,67 @@
-var express = require('express');
-var ParseServer = require('parse-server').ParseServer;
-var path = require('path');
+const express = require('express');
+const ParseServer = require('parse-server').ParseServer;
+const path = require('path');
+const bodyParser = require('body-parser');
 
-var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+const databaseUri = 'mongodb://localhost:27017/khaya-parse' // process.env.DATABASE_URI || process.env.MONGODB_URI;
 
 if (!databaseUri) {
 	console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
-var api = new ParseServer({
-	databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-	cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-	serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', 
-	appId: process.env.APP_ID || 'myAppId',
-	masterKey: process.env.MASTER_KEY || 'myMasterKey'
+const api = new ParseServer({
+	databaseURI: databaseUri,
+	cloud:  __dirname + '/cloud/main.js',
+	serverURL:  'http://localhost:1337/parse', 
+	appId: 'parse-khaya-app-ID',
+	masterKey: 'parse-khaya-master-KEY'
 });
-var app = express();
+const app = express();
 
 // Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
 // Serve the Parse API on the /parse URL prefix
-var mountPath = process.env.PARSE_MOUNT || '/parse';
+const mountPath = process.env.PARSE_MOUNT || '/parse';
+
+// Parse middleware
 app.use(mountPath, api);
+
+// Middleware
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function (req, res) {
 	res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
 });
+
+app.post('/signUp', async (req, res) => {
+  const {name, gender, email, phone, address, school, registrationId, password} = req.body;
+  
+  const user = new Parse.User();
+  user.set('username', name);
+  user.set('gender', gender);
+  user.set('email', email);
+  user.set('phone', phone);
+  user.set('address', address);
+  user.set('school', school);
+  user.set('registrationId', registrationId);
+  user.set('password', password);
+
+  try{
+    const response = await user.signUp();
+    res.json({
+      success : true,
+      profile : response});
+  }
+  catch(error){
+    console.log(error.message)
+    res.json({
+    success : false,
+    error : error.message})
+  }
+})
 
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
