ab1878258306a8e8f8e1737c2d18331657366933
Conceptx <theophilusokoye6@gmail.com>
2019-02-13 22:29:55 +0200

    Sync property changes to algolia

--- cloud/triggers/testObject.js
+++
@@ -1,7 +0,0 @@
-Parse.Cloud.beforeSave("TestObject", function(request, response) {
-  if (!request.object.get("foo")) {
-    response.error("foo must be set");
-  } else {
-    response.success();
-  }
-});
