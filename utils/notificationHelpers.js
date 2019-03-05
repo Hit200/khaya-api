3879c39bde3c96c7c3e5a2e1dee328fa7b92d242
Conceptx <theophilusokoye6@gmail.com>
2019-03-05 17:04:05 +0200

    get Min and Max

--- utils/notificationHelpers.js
+++ utils/notificationHelpers.js
@@ -1,12 +1,13 @@
-const accountSid = 'ACa53a74bbb275882fc8ffda66fb19a9c6'; //process.env.TWILIO_SID ;
-const authToken = '6871ffa01d925909efa15fc01d65dfe8'; //process.env.TWILIO_AUTH_TOKEN;
+const accountSid = process.env.TWILIO_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const { report } = require('../utils/errorHelpers');
 const client = require('twilio')(accountSid, authToken);
 
 const sendSMS = async () => {
 	try {
 		const message = await client.messages.create({
 			body: `Hie Faith,\n Your booking at 42 Castens Ave, Belvedere, Harare, Zimbabwe for room no. 1 was successful.`,
-			from: '+12543122731', //process.env.TWILIO_CONTACT,
+			from: process.env.TWILIO_CONTACT,
 			to: '+263778618403'
 		});
 		console.log(message);
